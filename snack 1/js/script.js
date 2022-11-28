1c69a24e22f8d4ccf47b9f493f97e187c2f3d721
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-28 16:50:31 +0100

    Snack 1

---
+++ snack 1/js/script.js
@@ -0,0 +1,8 @@
+
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+const userNumber = prompt ('Inserisci un numero da 1 a 10'); 
+while (userNumber > 10)  {
+    const userNumber = prompt ('Inserisci un numero da 1 a 10'); 
+}
+console.log(userNumber);
