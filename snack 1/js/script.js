71d46faff4c6ebcf0635f9e2287a1b5b05208a03
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-28 17:04:33 +0100

    Snack 1 - Change Js code

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -2,7 +2,19 @@
 // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
 
 const userNumber = prompt ('Inserisci un numero da 1 a 10'); 
-while (userNumber > 10)  {
+
+while ((userNumber > 10) || (userNumber < 1))  {
     const userNumber = prompt ('Inserisci un numero da 1 a 10'); 
 }
 console.log(userNumber);
+
+
+/**
+ * oppure
+ *
+let userNumber;
+
+do {
+    const userNumber = prompt ('Inserisci un numero da 1 a 10'); 
+    }  while ((userNumber > 10) || (userNumber < 1));
+ */
