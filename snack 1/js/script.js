7147ba34775375150718db0a475185df0916a9e5
giuliaballestrero <giulia.ballestrero@gmail.com>
2022-11-28 18:31:00 +0100

    Snack 1 - Adjust file error

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -1,20 +1,22 @@
 
 // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
 
-const userNumber = prompt ('Inserisci un numero da 1 a 10'); 
+/**let userNumber = prompt ('Inserisci un numero da 1 a 10'); 
 
 while ((userNumber > 10) || (userNumber < 1))  {
-    const userNumber = prompt ('Inserisci un numero da 1 a 10'); 
+    userNumber = prompt ('Inserisci un numero da 1 a 10'); 
 }
 console.log(userNumber);
-
+*/
 
 /**
  * oppure
- *
+ * */
+
 let userNumber;
 
 do {
-    const userNumber = prompt ('Inserisci un numero da 1 a 10'); 
+    userNumber = prompt ('Inserisci un numero da 1 a 10'); 
     }  while ((userNumber > 10) || (userNumber < 1));
- */
+
+    console.log(userNumber);
